13dbffc718f166aa4052909526da70f7bc8466d8
Loc Nguyen <loc.nguyen.leo@gmail.com>
2025-04-02 18:13:21 -0400

    page deploy fix

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/GameHub-FinalProject/",
+  base: "/gamehub/",
   plugins: [react()],
   build: {
     outDir: 'dist'
