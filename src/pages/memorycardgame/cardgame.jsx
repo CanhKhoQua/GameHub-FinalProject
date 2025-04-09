19aa6e9eaf21bb50ecce5666f124b2d452689337
Loc Nguyen <loc.nguyen.leo@gmail.com>
2025-04-09 17:41:43 -0400

    api fetch data sample

--- src/pages/memorycardgame/cardgame.jsx
+++ src/pages/memorycardgame/cardgame.jsx
@@ -5,13 +5,22 @@ import Form from "../../components/memorycardgame/Form";
 export default function Cardgame()
 {
     const [isGameOn, setIsGameOn] = useState(false);
+    const [emojiData, setEmojiData] = useState([]);
+    console.log(emojiData);
 
     async function startGame(e) {
         e.preventDefault();
         try {
             const res = await fetch(`https://emojihub.yurace.pro/api/all/category/animals-and-nature`);
+            if (!res.ok)
+            {
+                throw new Error ("Failed to fetch data from API")
+            }
             const data = await res.json();
-            console.log(data)
+            
+            const dataSample = data.slice(0,4)
+            setEmojiData(dataSample);
+
             setIsGameOn(true)
         } catch (e) {
             console.error(e)
