eb003156130344633732aa18211027704dc89dbb
winnieduong <khanhuyenhduong@gmail.com>
2025-04-14 15:43:02 -0400

    update layout

--- src/pages/hangman/hangman.jsx
+++ src/pages/hangman/hangman.jsx
@@ -1,10 +1,30 @@
-function Hangman()
+export default function Hangman()
 {
     return (
-        <>
-        <h1>Hangman</h1>
-        </>
+        <div className="hm-container">
+            <div className="hangman-box">
+                <img src="./hangman/assets/hangman-0.svg"></img>
+                <h1>Hangman</h1>
+            </div>
+            <div className="game-box">
+                <ul className="word-display">
+                    <li className="letter"></li>
+                    <li className="letter"></li>
+                    <li className="letter"></li>
+                    <li className="letter"></li>
+                    <li className="letter"></li>
+                    <li className="letter"></li>
+                    <li className="letter"></li>
+                </ul>
+                <h4 className="guess-text">
+                    Incorrect guesses:
+                    <b>0 / 6</b>
+                </h4>
+                <div className="keyboard">
+                    <button>a</button>
+                </div>
+            </div>
+        </div>
     )
 }
 
-export default Hangman;
